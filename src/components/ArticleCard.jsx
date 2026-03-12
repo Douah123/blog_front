003e8b1f53aad6c3cdf3bb7fb396f2d5fe2cd4ce
Mamadou Douah Barry <douahbarry123@gmail.com>
2026-03-12 17:48:12 +0000

    modif

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,7 +2,7 @@ function ArticleCard({ article }) {
   return (
     <article>
       <h3>{article?.title ?? 'Article'}</h3>
-      
+      <p>{article?.content ?? "Contenu de l'article."}</p>
     </article>
   )
 }
